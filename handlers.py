8276c5434a4e34c092f811e93edba2d04c41efd4
Jephthah Orobia <sirjhep@gmail.com>
2015-05-27 02:35:49 +0800

    Added query for classes (SEctions)

--- handlers.py
+++ handlers.py
@@ -24,6 +24,7 @@ class Handler(webapp2.RequestHandler):
         self.initialize(request, response)
         self.sys = SY.query()
         self.teachers = Teacher.query()
+        self.classes = Section.query()
 
     def write(self, *a, **kw):
         """Helper function for rendering htmls"""
